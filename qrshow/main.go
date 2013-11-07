1cc242e7dfdda821ec37a63ae0dc77c5196bd47c
Damian Gryski <damian@gryski.com>
2013-11-07 21:29:04 +0100

    qrshow: qr-codes in your terminal

---
+++ qrshow/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"code.google.com/p/rsc/qr"
+	"flag"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	level := flag.String("-level", "m", "QR level: L/M/Q/H")
+
+	flag.Parse()
+
+	var qrLevel qr.Level
+
+	switch *level {
+	case "L", "l":
+		qrLevel = qr.L
+	case "M", "m":
+		qrLevel = qr.M
+	case "H", "h":
+		qrLevel = qr.H
+	case "Q", "q":
+		qrLevel = qr.Q
+	default:
+		log.Fatal("unknown QR level:", *level)
+	}
+
+	data := flag.Arg(0)
+
+	code, _ := qr.Encode(data, qrLevel)
+
+	white := "\033[30;47m"
+	reset := "\033[0m"
+
+	fmt.Print(reset + white)
+
+	// two pixel border
+	for i := 0; i < 2; i++ {
+		fmt.Print(white + "    ")
+		for x := 0; x < code.Size; x++ {
+			fmt.Print("  ")
+		}
+		fmt.Println(white + "    " + reset)
+	}
+
+	for x := 0; x < code.Size; x++ {
+		fmt.Print(white + "    ") // two pixel border
+		for y := 0; y < code.Size; y++ {
+			// invert, actually
+			if code.Black(x, y) {
+				fmt.Print(reset + "  ")
+			} else {
+				fmt.Print(white + "  ")
+			}
+		}
+		fmt.Println(white + "    " + reset) // two pixel border
+	}
+
+	// two pixel border
+	for i := 0; i < 2; i++ {
+		fmt.Print(white + "    ")
+		for x := 0; x < code.Size; x++ {
+			fmt.Print("  ")
+		}
+		fmt.Println(white + "    " + reset)
+	}
+}
