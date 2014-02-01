f29cc147ff1b34601df38ee4c731d09537d0b21e
Damian Gryski <damian@gryski.com>
2014-02-01 16:48:02 +0100

    httpecho: improved response, instead of just dumping http.Request object

--- httpecho/main.go
+++ httpecho/main.go
@@ -1,7 +1,5 @@
 package main
 
-// TODO: reimplement useful features from http://httpbin.org/
-
 import (
 	"encoding/json"
 	"io/ioutil"
@@ -9,17 +7,56 @@ import (
 	"net/http"
 )
 
+type response struct {
+	Args map[string]string `json:"args"`
+	Data string            `json:"data"`
+	// File    map[string]string `json:"file"`
+	Form    map[string]string `json:"form"`
+	Headers map[string]string `json:"headers"`
+	URL     string            `json:"url"`
+	Json    interface{}       `json:"json"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	j, _ := json.Marshal(r)
 
-	//turn r.Body from a io.ReadCloser into the actual content
+	var resp response
+
+	r.ParseForm()
+
+	resp.Args = make(map[string]string)
+	for k, v := range r.URL.Query() {
+		resp.Args[k] = v[0]
+	}
+
 	body, _ := ioutil.ReadAll(r.Body)
-	var jr map[string]interface{}
-	json.Unmarshal(j, &jr)
-	jr["Body"] = string(body)
+	resp.Data = string(body)
 
-	j, _ = json.Marshal(jr)
+	resp.Form = make(map[string]string)
+	for k, v := range r.PostForm {
+		resp.Form[k] = v[0]
+	}
+
+	resp.Headers = make(map[string]string)
+	for k, v := range r.Header {
+		resp.Headers[k] = v[0]
+	}
+
+	resp.URL = r.URL.String()
+
+	if r.Header.Get("Content-Type") == "application/json" {
+		var j interface{}
+
+		err := json.Unmarshal(body, &j)
+		if err == nil {
+			resp.Json = j
+		} else {
+			// invalid JSON passed in body
+			resp.Json = "<invalid json>"
+		}
+	}
+
+	j, _ := json.Marshal(resp)
 	w.Write(j)
 }
 
