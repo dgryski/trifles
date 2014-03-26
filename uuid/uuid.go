a658c0f0ed413df6eb9e00add4907da378a1d76a
Damian Gryski <damian@gryski.com>
2014-03-26 11:46:25 +0100

    uuid: generate type 4 (random) uuids

---
+++ uuid/uuid.go
@@ -0,0 +1,21 @@
+package uuid
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"log"
+)
+
+// UUIDv4 returns a type 4 (`random') UUID
+func UUIDv4() string {
+	b := make([]byte, 16)
+	_, err := io.ReadFull(rand.Reader, b)
+	if err != nil {
+		// probably "shouldn't happen"
+		log.Fatal(err)
+	}
+	b[6] = (b[6] & 0x0F) | 0x40
+	b[8] = (b[8] &^ 0x40) | 0x80
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
