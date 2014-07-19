04781f1326375d476e4d6c7ef74e41f5a6b4e88a
Damian Gryski <damian@gryski.com>
2014-07-20 00:26:07 +0200

    cachetest: add slightly less dumb s4lru implementation

--- cachetest/main.go
+++ cachetest/main.go
@@ -3,6 +3,7 @@ package main
 /*
 
 slru2080: 6.36564715s 10000000 total 2254569 misses
+s4lru: 3.176211561s 10000000 total 2259629 misses
 lfu: 5.194561433s 10000000 total 2326371 misses
 slru5050: 6.266535668s 10000000 total 2360416 misses
 clock: 2.497958238s 10000000 total 2587380 misses
@@ -21,6 +22,7 @@ import (
 	"github.com/calmh/lfucache"
 	"github.com/dgryski/trifles/cachetest/clock"
 	"github.com/dgryski/trifles/cachetest/random"
+	"github.com/dgryski/trifles/cachetest/s4lru"
 	"github.com/dgryski/trifles/cachetest/slru"
 	"github.com/golang/groupcache/lru"
 )
@@ -107,11 +109,23 @@ func main() {
 		for in.Scan() {
 			domain := in.Text()
 			if i := cache.Get(domain); i == nil {
-				cache.Set(domain, domain)
-				i = domain
+				cache.Put(domain, domain)
 				miss += 1
 			}
+			count += 1
+		}
 
+	case "s4lru":
+
+		cache := s4lru.New(1000)
+
+		in := bufio.NewScanner(os.Stdin)
+		for in.Scan() {
+			domain := in.Text()
+			if i := cache.Get(domain); i == nil {
+				cache.Put(domain, domain)
+				miss += 1
+			}
 			count += 1
 		}
 	}
