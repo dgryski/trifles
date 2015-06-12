a8c34b0e9895753a45825b5343b76a972cf85ad6
Damian Gryski <damian@gryski.com>
2015-06-12 10:07:01 +0200

    bluniq: update to use bloomf instead of dgobloom

--- bluniq/main.go
+++ bluniq/main.go
@@ -2,59 +2,34 @@ package main
 
 import (
 	"bufio"
-	"crypto/rand"
-	"encoding/binary"
 	"flag"
-	"hash"
 	"log"
 	"os"
 
-	"github.com/dgryski/dgobloom"
-	"github.com/dgryski/dgohash"
+	"github.com/dchest/siphash"
+	"github.com/dgryski/go-bloomf"
 )
-
-type h32 struct {
-	hash.Hash64
-}
-
-func (h h32) Sum32() uint32 {
-	return uint32(h.Sum64())
-}
 
 func main() {
 
-	f := flag.String("f", "", "input file")
+	f := flag.String("f", "/dev/stdin", "input file")
 	n := flag.Int("n", 100e6, "cardinality estimate of set")
 	fprate := flag.Float64("fp", 0.00000001, "false positive rate")
 
 	flag.Parse()
-
-	if *f == "" {
-		flag.Usage()
-	}
 
 	file, err := os.Open(*f)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	var lines int
+	b := bloomf.New(*n, *fprate, func(b []byte) uint64 { return siphash.Hash(0, 0, b) })
 	scanner := bufio.NewScanner(file)
-
-	lines := 0
-
-	var salts []uint32
-
-	for i, needed := uint(0), dgobloom.SaltsRequired(uint32(*n), *fprate); i < needed; i++ {
-		var u [4]byte
-		rand.Read(u[:])
-		salts = append(salts, binary.LittleEndian.Uint32(u[:]))
-	}
-
-	b := dgobloom.NewBloomFilter(uint32(*n), *fprate, dgohash.NewMurmur3_x86_32(), salts)
 
 	for scanner.Scan() {
 		lines++
-		if ok := b.Exists(scanner.Bytes()); !ok {
+		if !b.Lookup(scanner.Bytes()) {
 			os.Stdout.Write(scanner.Bytes())
 			os.Stdout.Write([]byte("\n"))
 		}
