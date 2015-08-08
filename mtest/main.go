4ba2a114ae7fdca561a18052ec5fed221a7fe032
Damian Gryski <damian@gryski.com>
2015-08-08 13:04:18 +0200

    mtest: port of libtommath mtest program

---
+++ mtest/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var z big.Int
+	var a, b big.Int
+	var r big.Int
+
+	two := big.NewInt(2)
+
+	var n_add, n_sub, n_mul2, n_div2, n_gcd, n_sqr, n_invmod int
+
+	for scanner.Scan() {
+		cmd := scanner.Text()
+
+		fmt.Println(n_add, n_sub, n_mul2, n_div2, n_gcd, n_sqr, n_invmod)
+
+		switch cmd {
+
+		case "add_d", "add":
+			n_add++
+			next(scanner, &a)
+			next(scanner, &b)
+			next(scanner, &r)
+
+			z.Add(&a, &b)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+
+		case "sub_d", "sub":
+			n_sub++
+			next(scanner, &a)
+			next(scanner, &b)
+			next(scanner, &r)
+
+			z.Sub(&a, &b)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+
+		case "mul2":
+			n_mul2++
+			next(scanner, &a)
+			next(scanner, &r)
+
+			z.Mul(&a, two)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+
+		case "div2":
+			n_div2++
+			next(scanner, &a)
+			next(scanner, &r)
+
+			z.Div(&a, two)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+		case "sqr":
+			n_sqr++
+			next(scanner, &a)
+			next(scanner, &r)
+
+			z.Mul(&a, &a)
+			if z.Cmp(&r) != 0 {
+				fmt.Printf("sqr: %s %s %s\n", a.String(), r.String(), z.String())
+			}
+
+		case "gcd":
+			n_gcd++
+			next(scanner, &a)
+			next(scanner, &b)
+			next(scanner, &r)
+
+			z.GCD(nil, nil, &a, &b)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+
+		case "invmod":
+			n_invmod++
+			next(scanner, &a)
+			next(scanner, &b)
+			next(scanner, &r)
+
+			z.ModInverse(&a, &b)
+			if z.Cmp(&r) != 0 {
+				fmt.Println(a, b, r, z)
+			}
+
+		default:
+			// nothing
+		}
+	}
+}
+
+func next(scanner *bufio.Scanner, n *big.Int) {
+
+	ok := scanner.Scan()
+	if !ok {
+		panic("end of input")
+	}
+
+	txt := scanner.Text()
+
+	n.SetString(txt, 10)
+}
