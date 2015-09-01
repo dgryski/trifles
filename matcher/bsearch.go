a695f37125613d9e8a6676d4abca9720129ef324
Damian Gryski <damian@gryski.com>
2015-09-01 09:50:47 +0200

    matcher/bsearch: clean ups
    The sort package already includes a string sorting method -- we don't
    need to define our own slice type.
    
    Rename the generic `data` variable to be in line with other matcher types.

--- matcher/bsearch.go
+++ matcher/bsearch.go
@@ -7,14 +7,7 @@ import (
 	"sort"
 )
 
-type slice []string
-
-var data slice
-
-func (a slice) Len() int { return len(a) }
-
-func (a slice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a slice) Less(i, j int) bool { return a[i] < a[j] }
+var lookupBsearch []string
 
 func initBsearch() {
 
@@ -25,17 +18,17 @@ func initBsearch() {
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		data = append(data, scanner.Text())
+		lookupBsearch = append(lookupBsearch, scanner.Text())
 	}
-	sort.Sort(data)
+	sort.Strings(lookupBsearch)
 }
 
 func MatchBsearch(k []byte) bool {
 	in := string(k)
-	i := sort.SearchStrings(data, in)
+	i := sort.SearchStrings(lookupBsearch, in)
 	// the search fn gives the pos where it should be (if it's not there, where to insert it)
 	// so we still need to verify
-	if i < len(data) && data[i] == in {
+	if i < len(lookupBsearch) && lookupBsearch[i] == in {
 		return true
 	}
 	return false
