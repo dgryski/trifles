6bb5da4ad664d4fa5f25ccbe140a53bb635fe66b
Damian Gryski <damian@gryski.com>
2015-09-13 23:06:07 +0200

    numerical: secant: reduce repeated evaluations of f(x)
    This drops the number of ticks roughly in half: 6k -> 3k

--- numerical/root.go
+++ numerical/root.go
@@ -28,9 +28,12 @@ func secant(f func(x float64) float64, x0, x1 float64) (root float64) {
 
 	eps := 10e-8
 
+	fx0 := f(x0)
 	for math.Abs(x1-x0) > eps {
-		x2 := x1 - f(x1)*((x1-x0)/(f(x1)-f(x0)))
+		fx1 := f(x1)
+		x2 := x1 - fx1*((x1-x0)/(fx1-fx0))
 		x1, x0 = x2, x1
+		fx0 = fx1
 	}
 
 	return x1
