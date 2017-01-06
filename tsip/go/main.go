dc1200574e1250364ca3fb51c1aa79ed9f20c6da
Damian Gryski <dgryski@gmail.com>
2017-01-06 07:21:59 +0100

    tsip: add go implementation

---
+++ tsip/go/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/dchest/siphash"
+	"github.com/dgryski/go-marvin32"
+	"github.com/dgryski/go-sip13"
+)
+
+func main() {
+
+	bits := flag.Uint("s", 8, "table bits")
+	f := flag.String("f", "tsip", "function to test")
+
+	flag.Parse()
+
+	var h func(uint64, uint64, []byte) uint64
+
+	switch *f {
+	case "tsip":
+		h = tsip
+	case "siphash":
+		h = siphash.Hash
+	case "sip13":
+		h = sip13.Sum64
+	case "marvin32":
+		h = func(k0, k1 uint64, m []byte) uint64 { return uint64(marvin32.Sum32(0, m)) }
+	}
+
+	size := uint64(1<<*bits) - 1
+
+	t := make([]uint32, size+1)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		b := scanner.Bytes()
+		a := h(0, 0, b)
+		t[a&size]++
+	}
+
+	for i, v := range t {
+		fmt.Println(i, v)
+	}
+}
---
+++ tsip/go/tsip.go
@@ -0,0 +1,70 @@
+package main
+
+type sip struct {
+	v0, v1 uint64
+}
+
+func rotl64(x uint64, r uint64) uint64 {
+	return (((x) << (r)) | ((x) >> (64 - r)))
+}
+
+func (s *sip) round() {
+	s.v0 += s.v1
+	s.v1 = rotl64(s.v1, 13) ^ s.v0
+	s.v0 = rotl64(s.v0, 35) + s.v1
+	s.v1 = rotl64(s.v1, 17) ^ s.v0
+	s.v0 = rotl64(s.v1, 21) + s.v1
+	s.v1 += s.v0
+}
+
+func tsip(k0, k1 uint64, p []byte) uint64 {
+
+	s := sip{
+		v0: k0 ^ 0x736f6d6570736575,
+		v1: k1 ^ 0x646f72616e646f6d,
+	}
+	b := uint64(len(p)) << 56
+
+	for len(p) >= 8 {
+		m := uint64(p[0]) | uint64(p[1])<<8 | uint64(p[2])<<16 | uint64(p[3])<<24 | uint64(p[4])<<32 | uint64(p[5])<<40 | uint64(p[6])<<48 | uint64(p[7])<<56
+		s.v1 ^= m
+		s.round()
+		s.v0 ^= m
+		p = p[8:]
+	}
+
+	switch len(p) {
+	case 7:
+		b |= uint64(p[6]) << 48
+		fallthrough
+	case 6:
+		b |= uint64(p[5]) << 40
+		fallthrough
+	case 5:
+		b |= uint64(p[4]) << 32
+		fallthrough
+	case 4:
+		b |= uint64(p[3]) << 24
+		fallthrough
+	case 3:
+		b |= uint64(p[2]) << 16
+		fallthrough
+	case 2:
+		b |= uint64(p[1]) << 8
+		fallthrough
+	case 1:
+		b |= uint64(p[0])
+	}
+
+	// last block
+	s.v1 ^= b
+	s.round()
+	s.v0 ^= b
+
+	// finalization
+	s.v1 ^= 0xff
+	s.round()
+	s.round()
+
+	return s.v0 ^ s.v1
+}
