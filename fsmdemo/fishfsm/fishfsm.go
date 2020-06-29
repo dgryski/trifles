c160b71c392c04c50a259fb7ad9cf9155301fddf
Damian Gryski <damian@gryski.com>
2020-06-29 12:15:44 -0700

    fsmdemo: demo for libfsm code generation

---
+++ fsmdemo/fishfsm/fishfsm.go
@@ -0,0 +1,61 @@
+package fishfsm
+
+func Match(data []byte) int {
+	var idx = ^uint(0)
+
+l0:
+	if idx++; idx >= uint(len(data)) {
+		return -1
+	}
+
+	if data[idx] != 'f' {
+		goto l0
+	}
+
+l1: // e.g. "f"
+	if idx++; idx >= uint(len(data)) {
+		return -1
+	}
+
+	if data[idx] == 'f' {
+		goto l1
+	}
+
+	if data[idx] != 'i' {
+		goto l0
+	}
+
+l2: // e.g. "fi"
+	if idx++; idx >= uint(len(data)) {
+		return -1
+	}
+
+	if data[idx] == 'f' {
+		goto l1
+	}
+
+	if data[idx] == 'i' {
+		goto l2
+	}
+
+	if data[idx] != 's' {
+		goto l0
+	}
+
+	if idx++; idx >= uint(len(data)) {
+		return -1
+	}
+
+	if data[idx] == 'f' {
+		goto l1
+	}
+
+	if data[idx] != 'h' {
+		goto l0
+	}
+
+	{
+		return 4
+	}
+
+}
---
+++ fsmdemo/main.go
@@ -0,0 +1,18 @@
+package main
+
+//go:generate sh -c "re -r pcre -k str -e fish  -pl go 'fi+sh' >fishfsm/fishfsm.go"
+
+import (
+	"fmt"
+
+	"github.com/dgryski/trifles/fsmdemo/fishfsm"
+)
+
+func main() {
+	net := []string{"fsh", "catfish", "dogo", "fiiish"}
+	for _, caught := range net {
+		if fishfsm.Match([]byte(caught)) != -1 {
+			fmt.Println("caught a fish: ", caught)
+		}
+	}
+}
