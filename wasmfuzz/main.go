3e1ed5a309f404d54b5683e5590af81b702ba3c4
Damian Gryski <damian@gryski.com>
2022-07-26 21:04:46 -0700

    wasmfuzz: generate functions

--- wasmfuzz/main.go
+++ wasmfuzz/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"flag"
 	"io"
 	"math/rand"
 	"os"
@@ -14,7 +15,9 @@ type i32Op int
 const (
 	i32Add = iota
 	i32And
+	i32Call
 	i32Const
+	i32LocalGet
 	i32Mul
 	i32Or
 	i32RemS
@@ -30,8 +33,12 @@ func (op i32Op) String() string {
 		return "i32.add"
 	case i32And:
 		return "i32.and"
+	case i32Call:
+		return "call"
 	case i32Const:
 		return "i32.const"
+	case i32LocalGet:
+		return "local.get"
 	case i32Mul:
 		return "i32.mul"
 	case i32Or:
@@ -55,36 +62,14 @@ type Node struct {
 	i32  int32
 }
 
-func (op i32Op) eval(args []Node) int32 {
-	switch op {
-	case i32Add:
-		return args[0].i32 + args[1].i32
-	case i32And:
-		return args[0].i32 & args[1].i32
-	case i32Const:
-		return args[0].i32
-	case i32Mul:
-		return args[0].i32 * args[1].i32
-	case i32Or:
-		return args[0].i32 | args[1].i32
-	case i32RemS:
-		return args[0].i32 % args[1].i32
-	case i32Sub:
-		return args[0].i32 - args[1].i32
-	case i32Xor:
-		return args[0].i32 ^ args[1].i32
-	}
-	panic("unreached:" + op.String())
-}
-
 func (n *Node) Write(w io.Writer) {
-	if n.op == i32Const {
-		io.WriteString(w, n.op.String()+" "+strconv.FormatInt(int64(n.i32), 10)+"\n")
-		return
-	}
-
 	for _, arg := range n.args {
 		arg.Write(w)
+	}
+
+	if n.op == i32Const || n.op == i32LocalGet || n.op == i32Call {
+		io.WriteString(w, n.op.String()+" "+strconv.FormatInt(int64(n.i32), 10)+"\n")
+		return
 	}
 
 	io.WriteString(w, n.op.String()+"\n")
@@ -98,13 +83,31 @@ func (op i32Op) arity() int {
 	return 2
 }
 
-type generator struct {
-	fuel int
+type Func struct {
+	name  string
+	nargs int
+	body  Node
+	idx   int32
 }
 
-func (g *generator) i32() Node {
+type generator struct {
+	funcs []Func
+	fuel  int
+}
+
+func (g *generator) i32(f *Func) Node {
 
 	if g.fuel == 0 || rand.Intn(5) == 0 {
+
+		// sometimes generate a local.get
+		if rand.Intn(3) == 0 && f.nargs > 0 {
+			return Node{
+				op:  i32LocalGet,
+				i32: int32(rand.Intn(f.nargs)),
+			}
+		}
+
+		// generate a regular i32.const
 		n := Node{
 			op:  i32Const,
 			i32: rand.Int31(), // TODO(dgryski): generate edge values some of the time
@@ -124,27 +127,68 @@ func (g *generator) i32() Node {
 		var args []Node
 		nargs := op.arity()
 		for i := 0; i < nargs; i++ {
-			args = append(args, g.i32())
+			args = append(args, g.i32(f))
 		}
 
-		if op == i32RemS && args[1].i32 == 0 {
-			// insert another node to add 1 to avoid division by zero
-			constOne := Node{
-				op:  i32Const,
-				i32: 1, // TODO(dgryski): add random here?
-			}
-			addOne := Node{
-				op:   i32Add,
-				args: []Node{args[1], constOne},
-				i32:  args[1].i32 + constOne.i32,
-			}
-			args[1] = addOne
-		}
+		// ideally we'd be able to avoid divide-by-zero here, but ditching it for now;
+		// too hard to statically determine now that we've added function calls
 
 		return Node{
 			op:   op,
 			args: args,
-			i32:  op.eval(args),
+		}
+
+	case i32Call:
+		if rand.Intn(3) == 0 {
+			idx := int32(rand.Intn(len(g.funcs)))
+			if idx != f.idx {
+				// don't allow recursive calls
+				call := g.funcs[idx]
+				var args []Node
+				for i := 0; i < call.nargs; i++ {
+					args = append(args, g.i32(f))
+				}
+				n := Node{
+					op:   i32Call,
+					args: args,
+					i32:  idx,
+				}
+
+				return n
+			}
+		}
+
+		// generate a new function instead
+		nargs := rand.Intn(16)
+		newf := Func{
+			nargs: nargs,
+		}
+		newf.body = g.i32(&newf)
+		g.funcs = append(g.funcs, newf)
+		newf.idx = int32(len(g.funcs)) - 1
+
+		// now generate a call to f
+		var args []Node
+		for i := 0; i < newf.nargs; i++ {
+			args = append(args, g.i32(f))
+		}
+		return Node{
+			op:   op,
+			args: args,
+			i32:  newf.idx,
+		}
+
+	case i32LocalGet:
+		if f.nargs == 0 {
+			// no arguments to current function; generate a random const instead
+			return Node{
+				op:  i32Const,
+				i32: rand.Int31(), // TODO(dgryski): generate edge values some of the time
+			}
+		}
+		return Node{
+			op:  op,
+			i32: int32(rand.Intn(f.nargs)),
 		}
 
 	case i32Const:
@@ -157,15 +201,45 @@ func (g *generator) i32() Node {
 	panic("generate.i32: " + op.String())
 }
 
+func (g *generator) Write(w io.Writer) {
+	io.WriteString(w, "(module ")
+
+	for _, f := range g.funcs {
+		io.WriteString(w, "(func ")
+		if f.name != "" {
+			io.WriteString(w, `(export "`+f.name+`") `)
+		}
+		for i := 0; i < f.nargs; i++ {
+			io.WriteString(w, "(param i32) ")
+		}
+		io.WriteString(w, "(result i32) ")
+		io.WriteString(w, "\n")
+		f.body.Write(w)
+		io.WriteString(w, ")\n")
+	}
+
+	io.WriteString(w, ")\n")
+}
+
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println(`(module (func (export "_start")`)
-	fuel := rand.Intn(100)
-	g := generator{fuel: fuel}
-	n := g.i32()
-	n.Write(os.Stdout)
-	fmt.Println("i32.const " + strconv.FormatInt(int64(n.i32), 10))
-	fmt.Println("i32.ne")
-	fmt.Println("(if (then unreachable))")
-	fmt.Println(") )")
+	seed := flag.Int64("seed", time.Now().UnixNano(), "random seed")
+	flag.Parse()
+
+	rand.Seed(*seed)
+	fuel := rand.Intn(1000)
+	start := Func{
+		name:  "_start",
+		nargs: 0,
+		idx:   0,
+	}
+	g := generator{
+		fuel:  fuel,
+		funcs: []Func{start},
+	}
+
+	g.funcs[0].body = g.i32(&start)
+
+	b := bufio.NewWriter(os.Stdout)
+	g.Write(b)
+	b.Flush()
 }
