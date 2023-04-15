1d380192dfd0ecb1b7136cc6f660697935786e93
Damian Gryski <damian@gryski.com>
2023-04-14 19:16:49 -0700

    sshregexp: fix C tranpoline unsafe code

--- sshdregex/c/c.go
+++ sshdregex/c/c.go
@@ -8,7 +8,10 @@ import "C"
 import "unsafe"
 
 func Match(data []byte) int {
-	b := unsafe.Pointer(&data[0])
-	e := unsafe.Add(b, len(data))
+	var b, e unsafe.Pointer
+	if len(data) > 0 {
+		b = unsafe.Pointer(&data[0])
+		e = unsafe.Add(b, len(data))
+	}
 	return int(C.sshdmain((*C.char)(b), (*C.char)(e)))
 }
